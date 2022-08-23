81d4143ad63d8d63f4c5be65b7d0972d76afe327
Kutman7 <kutman.stel@gmail.com>
2022-08-23 18:29:21 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ryder/precache-manifest.62cc0a7831715908c172f3dfd9740b24.js"
+  "/ryder/precache-manifest.c7be07059105e9d81c41fc76f0daf89c.js"
 );
 
 workbox.clientsClaim();
